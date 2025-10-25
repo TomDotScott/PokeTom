c8ac2e951a71a3d3d0bf1273271d42c2428110ea
Tom Scott <tom.scott45456@gmail.com>
2025-10-26 00:43:56 +0100

    Move TMX files into the tiled_export folder Ensure all used tiles have flags set

--- Tiled Projects/hgss.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="hgss" tilewidth="32" tileheight="32" tilecount="4712" columns="8">
- <image source="../data/sprites/hgss_overworld.png" width="256" height="18848"/>
-</tileset>
--- data/tiled_export/Characters.tsx
+++ data/tiled_export/Characters.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Characters" tilewidth="32" tileheight="32" tilecount="2784" columns="32">
- <image source="../data/sprites/characters/overworld_entities.png" width="1024" height="2784"/>
+ <image source="../sprites/characters/overworld_entities.png" width="1024" height="2784"/>
 </tileset>
