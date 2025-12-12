569395d0db19b01f6887b2e5a4f1259daeef9f4c
Tom Scott <tom.scott45456@gmail.com>
2025-12-12 00:42:23 +0000

    Add new files to cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -11,6 +11,11 @@ echo ================================
 echo Removing Sharpmake build folders and generated projects...
 rd /s /q build_vs2022
 rd /s /q build
+rd /s /q obj
+rd /s /q output
+rd /s /q properties
+
+rd /s /q lib\lua\build_vs2022
 
 :: ----------------------
 :: Delete any generated VS files
@@ -20,15 +25,24 @@ for %%f in (*.sln *.vcxproj *.vcxproj.filters *.csproj sharpmakeautocleanupdb_de
     del /q "%%f"
 )
 
+for %%f in (src\Engine\CodeGen) do (
+    echo Deleting %%f
+    del /q "%%f"
+)
+
 :: ----------------------
-:: SFML build and libraries
+:: Libraries
 :: ----------------------
 echo Removing SFML build and libs...
 rd /s /q build_sfmldb
+rd /s /q SFML
 rd /s /q Lib\SFML\lib
 rd /s /q Lib\SFML\include
 rd /s /q Lib\SFML\share
 rd /s /q Lib\SFML\src
+
+echo Removing sol2 folder
+rd /s /q Lib\sol2
 
 echo Cleanup complete.
 pause
