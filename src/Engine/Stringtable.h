58a0e3d0264701867c80b2edf40675fef6d805f8
Tom Scott <tom.scott45456@gmail.com>
2026-06-08 22:43:19 +0100

    Add "Dynamic String" concept to the stringtables, replaced at runtime with values from a variadic function

--- src/Engine/Stringtable.h
+++ src/Engine/Stringtable.h
@@ -1,8 +1,10 @@
 #ifndef STRINGTABLE_H
 #define STRINGTABLE_H
+#include <regex>
 #include <string>
 #include <unordered_map>
 
+#include "Asserts.h"
 #include "Factory.h"
 #include "Hash.h"
 #include "Language.h"
@@ -18,6 +20,44 @@ public:
 	void AddCustomString(const hash_type& group, const hash_type& stringID, const std::string& value);
 	std::string GetString(const hash_type& stringID) const;
 	std::string GetString(const hash_type& group, const hash_type& stringID) const;
+
+	template <typename... Args>
+	std::string GetDynamicString(const hash_type& stringID, Args... fmt)
+	{
+		std::string baseString = GetString("DYNAMIC_STRING", stringID);
+
+		std::string result;
+		result.reserve(baseString.size() * 2);
+
+		// Parse the string for %sN and replace with the variadic arg
+		std::regex re(R"(%\d+)");
+
+		auto begin = std::sregex_iterator(baseString.begin(), baseString.end(), re);
+		auto end = std::sregex_iterator();
+
+		auto params = std::array{ fmt... };
+		std::size_t lastPos = 0;
+
+		for (auto i = begin; i != end; ++i)
+		{
+			const std::smatch& match = *i;
+
+			result.append(baseString, lastPos, match.position() - lastPos);
+
+			const std::string captured = match[0].str();
+
+			int idx = std::stoi(captured.substr(1));
+
+			result += params[idx - 1];
+
+			lastPos = match.position() + match.length();
+		}
+
+		// Append remainder
+		result.append(baseString, lastPos, std::string::npos);
+
+		return result;
+	}
 
 private:
 	// KEY - group, VALUE - { KEY - ID, VALUE - String }
